4662bc62a884895cd6141077ddcd0df89552f3aa
ÖMER FARUK YILMAZ <omerfaruk2441@gmail.com>
2024-09-28 14:10:44 +0300

    Task done. Tested

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,45 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if( isBarking && ((clock < 24 && clock > 16) || (clock < 8 && clock > 0)) ){
+            return true;
+        }
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if( (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19) ){
+            return true;
+        }
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if(isSummer == true){
+            if(temp >= 25 && temp <= 45){
+                return true;
+            }
+            return false;
+        }
+        else{
+            if(temp >= 25 && temp <= 35){
+                return true;
+            }
+            return false;
+        }
     }
 
     public static double area(double width, double height) {
-       
+        if(width < 0 || height < 0){
+            return -1;
+        }
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if(radius < 0 ){return  -1;}
+        return radius * radius * Math.PI;
     }
 }
